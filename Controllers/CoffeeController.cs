d3776c67701dc6c837bff4cf7533c7d36b6d294c
Brett Raper <72267696+techforchrist5227@users.noreply.github.com>
2023-05-21 14:09:31 -0400

    Added Controllers For Coffee

---
+++ Controllers/CoffeeController.cs
@@ -0,0 +1,22 @@
+﻿using HolyGroundsCoffee.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolyGroundsCoffee.Controllers
+{
+    public class CoffeeController : Controller
+    {
+        private readonly ICoffeeRepo _coffeeRepo;
+        private readonly ICoffeeCategoryRepo _coffeeCategoryRepo;
+
+        public CoffeeController(ICoffeeRepo coffeeRepo, ICoffeeCategoryRepo coffeeCategoryRepo)
+        {
+            _coffeeRepo = coffeeRepo;
+            _coffeeCategoryRepo = coffeeCategoryRepo;
+        }
+
+        public IActionResult List()
+        {
+            return View(_coffeeRepo.Coffees);
+        }
+    }
+}
